706557c5eda6e24c75cad777f6648829eecb432e
KenichiroArai <sammyleejunior@gmail.com>
2026-04-25 11:39:08 +0900

    リスト11-3-6。修正。

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,7 +11,7 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   {
-    ignores: ['scr/generated/**/*'],
+    ignores: ['scr/generated/prisma/**/*'],
   },
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
