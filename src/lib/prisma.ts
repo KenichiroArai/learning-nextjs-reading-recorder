ace1898d1904411c4d94eca7aa9678c9b738dbf7
KenichiroArai <sammyleejunior@gmail.com>
2026-04-26 12:16:03 +0900

    リスト11-3-7

---
+++ src/lib/prisma.ts
@@ -0,0 +1,12 @@
+import { PrismaClient } from '@/generated/prisma/client'
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient }
+
+// global.prisma上にPrismaクライアントが存在すれば再利用
+const prisma =
+    globalForPrisma.prisma || new PrismaClient({ log: ['query'] });
+
+// 非Production環境ではglobalForPrisma.prismaにオブジェクトを格納
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+export default prisma;
