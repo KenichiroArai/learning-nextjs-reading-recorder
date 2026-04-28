1f9f35249610b676149f054b4ec3eb871e3fa981
KenichiroArai <sammyleejunior@gmail.com>
2026-04-28 13:32:20 +0900

    リスト11-4-6

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,13 @@ const nextConfig: NextConfig = {
   experimental: {
     cacheComponents: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        hostname: 'books.google.com'
+      },
+    ]
+  },
 };
 
 export default nextConfig;
