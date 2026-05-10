c555e451903a193fde80bcffaf1592118ec47319
KenichiroArai <sammyleejunior@gmail.com>
2026-05-10 12:46:08 +0900

    PostgreSQLの対応。

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,20 +1,45 @@
 import { PrismaClient } from '@/generated/prisma/client'
-import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3'
 
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient | undefined
+import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3'
+import { PrismaPg } from '@prisma/adapter-pg'
+
+const globalForPrisma = globalThis as unknown as {
+  prisma?: PrismaClient
 }
 
-const adapter = new PrismaBetterSqlite3({
-  url: process.env.DATABASE_URL!,
-})
+function createPrismaClient() {
+  const databaseUrl = process.env.DATABASE_URL
 
-const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
+  if (!databaseUrl) {
+    throw new Error('DATABASE_URL is not defined')
+  }
+
+  // SQLite
+  if (databaseUrl.startsWith('file:')) {
+    const adapter = new PrismaBetterSqlite3({
+      url: databaseUrl,
+    })
+
+    return new PrismaClient({
+      adapter,
+      log: ['query'],
+    })
+  }
+
+  // PostgreSQL
+  const adapter = new PrismaPg({
+    connectionString: databaseUrl,
+  })
+
+  return new PrismaClient({
     adapter,
     log: ['query'],
   })
+}
+
+const prisma =
+  globalForPrisma.prisma ??
+  createPrismaClient()
 
 if (process.env.NODE_ENV !== 'production') {
   globalForPrisma.prisma = prisma
