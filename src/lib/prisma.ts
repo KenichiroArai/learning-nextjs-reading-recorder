7f5ccf1a4f82d98809e96bdf8179ce750ec9f5bb
KenichiroArai <sammyleejunior@gmail.com>
2026-05-15 00:35:53 +0900

    prismaをv6.13.0に変更した。

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,48 +1,10 @@
 import { PrismaClient } from '@/generated/prisma/client'
 
-import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3'
-import { PrismaPg } from '@prisma/adapter-pg'
-
-const globalForPrisma = globalThis as unknown as {
-  prisma?: PrismaClient
-}
-
-function createPrismaClient() {
-  const databaseUrl = process.env.DATABASE_URL
-
-  if (!databaseUrl) {
-    throw new Error('DATABASE_URL is not defined')
-  }
-
-  // SQLite
-  if (databaseUrl.startsWith('file:')) {
-    const adapter = new PrismaBetterSqlite3({
-      url: databaseUrl,
-    })
-
-    return new PrismaClient({
-      adapter,
-      log: ['query'],
-    })
-  }
-
-  // PostgreSQL
-  const adapter = new PrismaPg({
-    connectionString: databaseUrl,
-  })
-
-  return new PrismaClient({
-    adapter,
-    log: ['query'],
-  })
-}
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 const prisma =
-  globalForPrisma.prisma ??
-  createPrismaClient()
+  globalForPrisma.prisma || new PrismaClient({ log: ['query'] });
 
-if (process.env.NODE_ENV !== 'production') {
-  globalForPrisma.prisma = prisma
-}
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
-export default prisma
+export default prisma;
