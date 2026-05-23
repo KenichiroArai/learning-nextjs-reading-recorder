3880e1d0d5a096aee4f194d4989db5b7bf58e044
KenichiroArai <sammyleejunior@gmail.com>
2026-05-24 01:17:19 +0900

    ESLintの設定を更新して、無視するディレクトリを「scr/generated/prisma/**/*」から「src/generated/prisma/**」に変更します。

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,7 +11,7 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   {
-    ignores: ['scr/generated/prisma/**/*'],
+    ignores: ['src/generated/prisma/**'],
   },
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
